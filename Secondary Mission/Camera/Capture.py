d0bd8c6c2386dde5492c6fdd0c846dc4a4d00075
Konstantinos Chantzis <chantzkonstantinos@gmail.com>
2026-01-08 20:37:01 +0000

    Made and Tested Capture.py. Made __init__.py for Camera package

---
+++ Secondary Mission/Camera/Capture.py
@@ -0,0 +1,24 @@
+from picamera2 import Picamera2
+from libcamera import Transform
+from PIL import Image
+import time
+
+cam = Picamera2()
+def ConfigureCamera():
+    cameraConfig = cam.create_still_configuration(
+    main={"size": (1024, 1024)}
+    )
+    cam.configure(cameraConfig)
+
+def StartCamera(startDelay):
+    cam.start()
+    time.sleep(startDelay)
+def StopCamera():
+    cam.stop()
+
+
+def CapturePhoto():
+    image = cam.capture_array()
+    img = Image.fromarray(image)
+    img.save("Capture.jpg", format="JPEG")
+
---
+++ Secondary Mission/Camera/__init__.py
@@ -0,0 +1,2 @@
+from .Capture import ConfigureCamera, StartCamera, StopCamera, CapturePhoto
+print("Initialized Camera Package")
